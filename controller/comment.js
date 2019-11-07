84ba782791bece8c84ac5f4eb843f0de962f0ced
Bilal Bellafkir <bilal.bellafkir@informatik.hu-berlin.de>
2019-11-07 11:44:13 +0100

    favorite CRUD finnished

--- controller/comment.js
+++ controller/comment.js
@@ -1,50 +1,24 @@
 const initConnection = require("../config/connectMysql").initConnection;
 
-// @desc get all comments
-// @route GET /api/v1/comment
-// @access Private
-//TODO: pagination + sorting
-exports.getComments = (req, res) => {
-  const conn = initConnection();
-  conn.query(
-    "SELECT * FROM comments p JOIN locations l ON p.id=l.comment_id JOIN descriptions d ON p.id=d.comment_id;",
-    (error, comments) => {
-      if (error) {
-        res.status(400).json({
-          success: false,
-          message: error.message
-        });
-      }
-      res.status(200).json({
-        success: true,
-        data: comments
-      });
-    }
-  );
-};
-
 // @desc get comment by id
 // @route GET /api/v1/comment/:id
 // @access Public
-exports.getCommentById = (req, res) => {
-  const { id } = req.params;
+exports.getCommentsByPooleventId = (req, res) => {
+  const { pooleventId } = req.params;
   const conn = initConnection();
-  conn.query(
-    `SELECT * FROM comments p WHERE p.id='${id}';`,
-    (err, comment) => {
-      if (err) {
-        res.status(400).json({
-          success: false,
-          message: `Error in getcommentById: ${err.message}`
-        });
-      } else {
-        res.status(200).json({
-          success: true,
-          data: comment
-        });
-      }
+  conn.query(`SELECT * FROM comments c WHERE c.poolevent_id='${pooleventId}';`, (err, comment) => {
+    if (err) {
+      res.status(400).json({
+        success: false,
+        message: `Error in getCommentsByPooleventId: ${err.message}`
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: comment
+      });
     }
-  );
+  });
 };
 
 // @desc  create comment
@@ -56,9 +30,15 @@ exports.postComment = (req, res) => {
   let conn = initConnection();
   conn.query(`INSERT INTO comments SET ?`, body, (error, comment) => {
     if (error) {
-      res.status(400).json({ success: false, messaage: error.message });
+      res.status(400).json({
+        success: false,
+        messaage: error.message
+      });
     } else {
-      res.status(200).json({ success: true, data: comment });
+      res.status(200).json({
+        success: true,
+        data: comment
+      });
     }
   });
 };
@@ -69,22 +49,19 @@ exports.postComment = (req, res) => {
 exports.deleteComment = (req, res) => {
   const { id } = req.params;
   const conn = initConnection();
-  conn.query(
-    `DELETE FROM  WHERE comments.id='${id}';`,
-    (error, resp) => {
-      if (error) {
-        res.status(400).json({
-          success: false,
-          message: `Error in deletecomment ${error.message}`
-        });
-      } else {
-        res.status(200).json({
-          success: true,
-          data: resp
-        });
-      }
+  conn.query(`DELETE FROM WHERE comments.id='${id}';`, (error, resp) => {
+    if (error) {
+      res.status(400).json({
+        success: false,
+        message: `Error in deletecomment ${error.message}`
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: resp
+      });
     }
-  );
+  });
 };
 
 // @desc edit comment by id
@@ -94,21 +71,19 @@ exports.putComment = (req, res) => {
   const { body } = req;
   const { id } = req.params;
   const conn = initConnection();
-  conn.query(
-    `UPDATE comments SET ? WHERE id =${id};`,
-    body,
-    (error, resp) => {
-      if (error) {
-        res.status(400).json({
-          success: false,
-          message: `Error in putcomment: ${error.message}`
-        });
-      } else {
-        res.status(200).json({
-          success: true,
-          data: resp
-        });
-      }
+  conn.query(`UPDATE comments SET ? WHERE id =${id};`, body, (error, resp) => {
+    if (error) {
+      res.status(400).json({
+        success: false,
+        message: `Error in putcomment: ${error.message}`
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: resp
+      });
     }
-  );
+  });
 };
+
+//TODO: getComments by user Id
---
+++ controller/favorites.js
@@ -0,0 +1,68 @@
+const initConnection = require("../config/connectMysql").initConnection;
+
+// @desc get favorite by userId
+// @route GET /api/v1/favorite/:id
+// @access Public
+exports.getFavoriteByUserId = (req, res) => {
+  const { userId } = req.params;
+  const conn = initConnection();
+  const sql = `SELECT * FROM favorites WHERE favorites.user_id='${userId}';`;
+  conn.query(sql, (err, favorites) => {
+    if (err) {
+      res.status(400).json({
+        success: false,
+        message: `Error in getFavoriteByUserId: ${err.message}`
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: favorites
+      });
+    }
+  });
+};
+
+// @desc  create favorite
+// @route POST /api/v1/favorite
+// @access Private
+//TODO: desc
+exports.postFavorite = (req, res) => {
+  const { body } = req;
+  let conn = initConnection();
+  const sql = `INSERT INTO favorites SET ?`;
+  conn.query(sql, body, (error, favorite) => {
+    if (error) {
+      res.status(400).json({
+        success: false,
+        messaage: error.message
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: favorite
+      });
+    }
+  });
+};
+
+// @desc delete favorite by id
+// @route DELETE /api/v1/favorite/:id
+// @access Private
+exports.deleteFavorite = (req, res) => {
+  const { id } = req.params;
+  const conn = initConnection();
+  const sql = `DELETE FROM favorites WHERE favorites.id='${id}';`;
+  conn.query(sql, (error, resp) => {
+    if (error) {
+      res.status(400).json({
+        success: false,
+        message: `Error in deletefavorite ${error.message}`
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: resp
+      });
+    }
+  });
+};
